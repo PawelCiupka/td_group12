83fabe0ce3635395ad9eb630d4d85234f107552b
Paweł Ciupka <pawelciupka@gmail.com>
2018-12-19 19:32:35 +0100

    Create simple project of view template.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,73 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      fetching: true
+      gameTitle: 'TD_GROUP12'
     };
   }
 
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          { process.env.NODE_ENV === 'production' ?
-              <p>
-                BEKA
-              </p>
-            : <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-          }
-          <p>{'« '}<strong>
-            {this.state.fetching
-              ? 'Fetching message from API'
-              : this.state.message}
-          </strong>{' »'}</p>
-          <p><a
-            className="App-link"
-            href="https://github.com/mars/heroku-cra-node"
-          >
-            React + Node deployment on Heroku
-          </a></p>
-          <p><a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a></p>
-        </header>
+
+        <div className="row first">
+
+          <div className="row gameTitle">
+            <div className="col-sm-12">
+              <h1>{this.state.gameTitle}</h1>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="row second">          
+          <div className="row">
+            <div className="col-sm-2"></div>
+
+            <div className="col-sm-8">
+              <div className="sectionTitle">
+                <h2>Walka</h2>
+              </div>
+            </div>
+
+            <div className="col-sm-2"></div>
+          </div>
+        </div>
+
       </div>
-    );
-  }
+  
+      
+    )}
 }
 
 export default App;
